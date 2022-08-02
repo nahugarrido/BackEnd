bde168457e4851dd5f3397b8eeabb5ef383799c0
Nahuel <68355186+nahugarrido@users.noreply.github.com>
2022-08-02 09:32:56 -0300

    cambio en entidad contacto

--- src/main/java/com/portfolio/nahuelgarrido/Entity/Contacto.java
+++ src/main/java/com/portfolio/nahuelgarrido/Entity/Contacto.java
@@ -15,16 +15,12 @@ public class Contacto {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
-    
-    @NotNull
-    @Size(min = 1, max = 100, message = "no cumple con la longitud")
-    private String mail_contacto;
 
-    @NotNull
+    
     @Size(min = 1, max = 100, message = "no cumple con la longitud")    
     private String github_url;
 
-    @NotNull
+    
     @Size(min = 1, max = 100, message = "no cumple con la longitud")    
     private String linkedin_url;
     
